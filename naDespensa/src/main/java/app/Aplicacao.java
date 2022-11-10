1d5528fba7dfdf4336b816684c1e8e090a3c9d42
waands <wanderjunior36@gmail.com>
2022-11-10 20:47:43 -0300

    spark access control allowed

--- naDespensa/src/main/java/app/Aplicacao.java
+++ naDespensa/src/main/java/app/Aplicacao.java
@@ -29,7 +29,27 @@ public class Aplicacao {
 		get("/listingredientes", (request, response) -> ingredienteService.getAll(request, response));
 		get("/getIngredienteApp/:k", (request, response) -> ingredienteService.getIngredienteApp(request.params(":k")));
 		
-		
+		options("/*",
+		        (request, response) -> {
+
+		            String accessControlRequestHeaders = request
+		                    .headers("Access-Control-Request-Headers");
+		            if (accessControlRequestHeaders != null) {
+		                response.header("Access-Control-Allow-Headers",
+		                        accessControlRequestHeaders);
+		            }
+
+		            String accessControlRequestMethod = request
+		                    .headers("Access-Control-Request-Method");
+		            if (accessControlRequestMethod != null) {
+		                response.header("Access-Control-Allow-Methods",
+		                        accessControlRequestMethod);
+		            }
+
+		            return "OK";
+		        });
+
+		before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 	}
 	/*
 	public static String getIngredienteApp (int k) {
